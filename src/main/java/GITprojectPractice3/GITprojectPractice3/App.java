aa5109a3d24a8e9dacd1c63efc90ec6ac46fc342
NataLiaKech <nkechedzhieva@gmail.com>
2020-07-03 21:44:33 -0400

    practice git3

---
+++ src/main/java/GITprojectPractice3/GITprojectPractice3/App.java
@@ -0,0 +1,13 @@
+package GITprojectPractice3.GITprojectPractice3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
